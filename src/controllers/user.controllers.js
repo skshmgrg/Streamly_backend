942f695cb8e20a78eb4c273408978c0f05b25bd3
skshmgrg <sakshamgarg3050@gmail.com>
2025-07-20 00:48:36 +0530

    fix: correct sameSite attribute value in cookie options

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -241,7 +241,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     const options = {
       httpOnly: true,
       secure: true,
-      sameSite:none
+      sameSite:"none"
     };
 
     const { accessToken, refreshToken } =
